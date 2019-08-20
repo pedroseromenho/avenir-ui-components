65d92fef50973eba41d18a20ca7d0a45bc977079
Pedro Seromenho <poso02@core.local>
2019-08-20 13:35:09 +0200

    init

---
+++ src/Components/Button/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from "./style.module.scss";
+
+function Button (){
+    return(
+        <div>
+            text
+        </div>
+    )
+}
+
+Button.propTypes = {
+
+};
+
+export default Button;
