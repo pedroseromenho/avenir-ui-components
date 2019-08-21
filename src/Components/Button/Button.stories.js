9b63d6a30a97f82ac4f5b4ca6af50c54917e44bb
Pedro Seromenho <poso02@core.local>
2019-08-21 11:47:49 +0200

    button sizes & types

--- src/Components/Button/Button.stories.js
+++ src/Components/Button/Button.stories.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import {
+  faCoffee, faUser, faCheck, faArrowLeft,
+} from '@fortawesome/free-solid-svg-icons';
 
 import Button from './index';
 
 const faker = require('faker');
 
+const icons = [faCoffee, faUser, faCheck, faArrowLeft];
+const icon = icons[Math.floor(Math.random() * icons.length)];
 
 storiesOf('Button', module)
-  .add('default', () => <Button text={faker.fake('{{lorem.word}}')} />)
-  .add('primary', () => <Button text={faker.fake('{{lorem.word}}')} type="primary" />);
+  .add('default', () => <Button text={faker.fake('{{name.title}}')} />)
+  .add('primary', () => <Button text={faker.fake('{{name.title}}')} type="primary" />)
+  .add('secondary', () => <Button text={faker.fake('{{name.title}}')} type="secondary" />)
+  .add('w/ icon', () => <Button text={faker.fake('{{name.title}}')} icon={icon} />)
+  .add('primary w/ icon', () => <Button text={faker.fake('{{name.title}}')} type="primary" icon={icon} />)
+  .add('small', () => <Button text={faker.fake('{{name.title}}')} />)
+  .add('medium', () => <Button text={faker.fake('{{name.title}}')} size="medium" />)
+  .add('big', () => <Button text={faker.fake('{{name.title}}')} size="big" />);
